c34f305ae2a355d2fe577058ff4f5920d3530ece
72fcosta <inbox5372@gmail.com>
2021-05-04 09:33:10 -0300

    ajustes

--- src/utils/dateValidation.js
+++ src/utils/dateValidation.js
@@ -9,11 +9,11 @@ const dateValidation = (value, previousDays = 0, nextDays = 30) => {
    previousDays = previousDays * 86400000
 
    const previousMs = new Date().getTime() - previousDays
-   previousMs.setHours(0, 0, 0, 0)
-
    const nextMs = new Date().getTime() + nextDays
 
    const previousDate = new Date(previousMs)
+   previousDate.setHours(0, 0, 0, 0)
+
    const nextDate = new Date(nextMs)
 
    dateValidation = valueToDate(value) >= previousDate && valueToDate(value) <= nextDate
